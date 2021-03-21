13a63483cc2d41df1c6dcad76ad8869562557561
jeroenvandevelde <jeroen.vandevelde@live.be>
2021-03-21 20:40:23 +0100

    used constructor injection

--- src/main/java/com/jeroen/howtowritetests/a8spring/BouncyCastleClient.java
+++ src/main/java/com/jeroen/howtowritetests/a8spring/BouncyCastleClient.java
@@ -1,7 +1,6 @@
 package com.jeroen.howtowritetests.a8spring;
 
 import com.jeroen.howtowritetests.a1naming.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,11 +8,15 @@ import java.util.Map;
 
 @Component
 public class BouncyCastleClient implements IBouncyCastleClient {
-    @Autowired
-    RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public BouncyCastleClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
 
     @Override
     public Person getPerson() {
-        return restTemplate.getForObject("http://localhost:8080/person?id={id}", Person.class, Map.of("id","123"));
+        return restTemplate.getForObject("http://localhost:8080/person?id={id}", Person.class, Map.of("id", "123"));
     }
 }
